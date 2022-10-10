152611f70cf4cef97826f286b22be54d2d2df027
Tasneem24 <makafanetshidi24@gmail.com>
2022-10-10 15:59:22 +0200

    initial commit

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,49 @@
-class Calculator{
-    add(numbers){
+add ("")
+add (0)
+add (2)
+add (1,2)
 
-    }
+function add(numbers){
+    let emptystring = ("")
+
+if (numbers === emptystring){
+    results = "Value is empty"
+}
+else{
+
+    results = "Value is positive";
+}
+ console.log(results)
+
+}
+console.log('');
+console.log(sum2("2,4"));
+
+
+function sum2(numbers){
+
+    
+
+    let emptystring =("");
+    let results = 0;
+    let myArray = numbers.split(",")
+
+ if (numbers === emptystring){
+   results = 0;
+} 
+else { 
+    results = parseInt(myArray[0]) + parseInt(myArray[1])
 }
 
-module.exports = Calculator
+//console.log(result);
+
+return results;
+} 
+
+function addUnkownNumbers(){
+  
+} 
+function addNewLineNumbers(){   
+
+}
+
